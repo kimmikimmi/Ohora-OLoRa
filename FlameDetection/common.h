7b4065f4bd8d7ad48fa86a6909ae39bd511ce871
Jaden <bright2013@icloud.com>
2016-09-11 20:40:35 +0900

    opencv flame detector

---
+++ FlameDetection/common.h
@@ -0,0 +1,35 @@
+//
+//  common.h
+//  FlameDetection
+
+#ifndef FlameDetection_common_h
+#define FlameDetection_common_h
+
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <list>
+#include <map>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/video.hpp>
+#include <opencv2/ml/ml.hpp>
+
+using namespace std;
+using namespace cv;
+
+//#define OLD_ALGO
+//#define TRAIN_MODE
+//#define DEBUG_OUTPUT
+
+#ifdef TRAIN_MODE
+extern bool trainComplete;
+#endif
+
+#endif
